5f5ea3d4af498d3328d38af50fe8b56b2603f7c5
Tanya Kolosenko <tanya.kolosenko.1705@gmail.com>
2023-10-08 19:46:49 +0300

    In Order class create variables and methods:AddOrder and GetStatus

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    public void Order() {
+        orderItems = new ArrayList<>();
+    }
+
+    List<Product> orderItems;
+    private static int orderId = 0;
+    private String status = "In progress";
+
+    public void AddOrder(Cart cart) {
+        orderId++;
+        status = "Complete";
+        orderItems.addAll(cart.listOfProducts);
+        cart.listOfProducts.clear();
+    }
+    public void GetStatus()
+    {
+        System.out.println(status);
+    }
 }
