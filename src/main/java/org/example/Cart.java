3c8094bebb380311394e3f943fcc489ae04c3489
Tanya Kolosenko <tanya.kolosenko.1705@gmail.com>
2023-10-08 19:32:45 +0300

    Create add and remove methods in class Cart Add Product class examples in Main

--- src/main/java/org/example/Cart.java
+++ src/main/java/org/example/Cart.java
@@ -1,8 +1,20 @@
 package org.example;
+import java.util.ArrayList;
+import java.util.List;
+public class Cart {
+    public Cart() {
+        listOfProducts = new ArrayList<>(); // Initialize the list in the constructor
+    }
 
-public class Cart   {
-
-
+    List<Product> listOfProducts;
+    void AddProductToCart(Product product)
+    {
+        listOfProducts.add(product);
+    }
+    void RemoveProductFromCart(Product product)
+    {
+        listOfProducts.remove(product);
+    }
 
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-    Product first = new Product();
-    first.AddProduct("Egg",3.2f);
+        Product first = new Product();
+        first.AddProduct("Egg", 3.2f);
 
+        Product second = new Product();
+        first.AddProduct("Mild", 32.9f);
+
+        Product third = new Product();
+        first.AddProduct("Bread", 15f);
+
+        Cart firstCart = new Cart();
+        firstCart.AddProductToCart(first);
     }
 }
