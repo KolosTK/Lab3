c96f538f42691bf9b49c829b69f2ce74d8be3da4
Tanya Kolosenko <tanya.kolosenko.1705@gmail.com>
2023-10-10 18:53:11 +0300

    Change syntax mistakes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
         first.AddProduct("Egg", 3.2f);
 
         Product second = new Product();
-        first.AddProduct("Mild", 32.9f);
+        first.AddProduct("Milk", 32.9f);
 
         Product third = new Product();
         first.AddProduct("Bread", 15f);
