5b83339031fa6f12bb680a16d3c9c47f7183b155
Tanya Kolosenko <tanya.kolosenko.1705@gmail.com>
2023-10-10 20:16:11 +0300

    Create test AddAndRemoveProductsFromCart which check correctness of functions AddProductToCart and  RemoveProductFromCart in class Cart

---
+++ src/test/java/org/example/AddAndRemoveProductsFromCart.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AddAndRemoveProductsFromCart {
+    Product first = new Product();
+    Cart firstCart = new Cart();
+
+    @Test
+    void addEggToCart() {
+        first.AddProduct("Egg", 3.2f);
+        firstCart.AddProductToCart(first);
+
+        assertTrue(firstCart.listOfProducts.contains(first));
+    }
+
+    @Test
+    void removeEggFromCart() {
+        first.AddProduct("Egg", 3.2f);
+
+        firstCart.AddProductToCart(first);
+        firstCart.RemoveProductFromCart(first);
+
+        assertFalse(firstCart.listOfProducts.contains(first));
+    }
+}
