0291b7187b3dcab671eff812aa6a94fa3bb4c371
Tanya Kolosenko <tanya.kolosenko.1705@gmail.com>
2023-10-10 21:12:28 +0300

    Create test AddOrderTest which check correctness of functions AddOrder in class Order

---
+++ src/test/java/org/example/AddOrderTest.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddOrderTest {
+    Product first = new Product();
+    Product second = new Product();
+    Product third = new Product();
+    Cart firstCart = new Cart();
+
+    Order firstOrder = new Order();
+
+    @BeforeEach
+    public void init() {
+
+        first.AddProduct("Egg", 3.2f);
+        first.AddProduct("Milk", 32.9f);
+        first.AddProduct("Bread", 15f);
+
+        firstCart.AddProductToCart(first);
+        firstCart.AddProductToCart(second);
+        firstCart.AddProductToCart(third);
+    }
+
+    @Test
+    void CheckForIncludingAllElements() {
+
+        firstOrder.AddOrder(firstCart);
+
+        assertTrue(firstOrder.orderItems.contains(first) &&
+                firstOrder.orderItems.contains(second) &&
+                firstOrder.orderItems.contains(third));
+    }
+
+    @Test
+    void IsFunctionComplete()
+    {
+        firstOrder.AddOrder(firstCart);
+        assertEquals("Complete",firstOrder.status);
+    }
+
+    @Test
+    void IsFunctionInProgress()
+    {
+        assertEquals("In progress",firstOrder.status);
+    }
+}
